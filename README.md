1a6c985684b9abda7b951b675d9553a64afc8f99
Sunny0025-IT <Sunny0025-IT@users.noreply.github.com>
2026-05-21 13:42:47 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+[![MapVina Logo](https://avatars.githubusercontent.com/u/282217237?s=400&u=edd583b269cdb491b5775bdf10978cde217b841e&v=4)](https://mapvina.com/)
+
+# MapVina Gestures for Android
+
+[![](https://img.shields.io/badge/Slack-%23mapvina--native-2EB67D?logo=slack)](https://slack.openstreetmap.us/)
+
+The MapVina Gestures for Android library wraps [GestureDetectorCompat](https://developer.android.com/reference/android/support/v4/view/GestureDetectorCompat.html) and introduces implementation of scale, rotate, move, shove and tap gesture detectors. MapVina Gestures for Android was inspired by [Android Gesture Detector Framework](https://github.com/Almeros/android-gesture-detectors) and offers the same functionality with some additional features on top.
+
+This is a dependency of [MapVina Native for Android](https://github.com/mapvina/mapvina-native/tree/main/platform/android#readme).
+
+## License
+
+**MapVina Gestures Android** is licensed under the [BSD 2-Clause License](./LICENSE.md).
---
+++ app/src/main/java/org/maplibre/android/gestures/testapp/MapboxActivity.java
@@ -0,0 +1,31 @@
+package com.mapvina.android.gestures.testapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mapvina.android.MapVina;
+import com.mapvina.android.maps.MapVinaMap;
+import com.mapvina.android.maps.MapView;
+import com.mapvina.android.maps.OnMapReadyCallback;
+
+/**
+ * Test activity showcasing a simple MapView with current mapbox-gestures-android library commit.
+ */
+public class MapboxActivity extends AppCompatActivity implements OnMapReadyCallback {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    MapVina.getInstance(this);
+
+    setContentView(R.layout.activity_mapbox);
+    MapView mapView = findViewById(R.id.map_view);
+    mapView.getMapAsync(this);
+  }
+
+  @Override
+  public void onMapReady(@NonNull MapVinaMap mapVinaMap) {
+    mapVinaMap.setStyle("https://maps.mapvina.com/styles/v1/streets.json?key=public_key");
+  }
+}
